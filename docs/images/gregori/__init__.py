c4c8c78d42561c79ec07339bb1d5d838a9ddeedf
N. Mustafaraj <149283993+nmustafaraj@users.noreply.github.com>
2026-08-23 00:51:38 +0200

    Delete docs/images directory

--- docs/images/gregori/__init__.py
+++
@@ -1,9 +0,0 @@
-"""GReGOrI: Genomic Repeat Grouping & Orientation Identifier.
-
-High-throughput identification, dynamic programming alignment, gene superimposition,
-and interactive visualization of Strict complementarity Hairpin-like Nested Elements (SHaNEs).
-"""
-from __future__ import annotations
-
-__version__ = "0.9.5"
-__author__ = "Neim Mustafaraj"
--- docs/images/gregori/__main__.py
+++
@@ -1,8 +0,0 @@
-"""Main entry point for executing `python -m gregori`."""
-from __future__ import annotations
-
-import sys
-from .cli import main
-
-if __name__ == "__main__":
-    main(sys.argv[1:])
