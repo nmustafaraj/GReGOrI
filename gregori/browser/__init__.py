4775725cb09548a75c58b04ab997061cc246350f
N. Mustafaraj <149283993+nmustafaraj@users.noreply.github.com>
2026-08-23 00:52:09 +0200

    Delete gregori directory

--- gregori/browser/__init__.py
+++
@@ -1,9 +0,0 @@
-"""GReGOrI SHaNE Browser Suite."""
-from __future__ import annotations
-
-from .builder import (
-    build_browser,
-    build_ehab_comparison_browser,
-    build_ehab_draft,
-    build_library,
-)
